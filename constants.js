f2c07b11c5c65840ca8a5df5753f6d37971c5af3
Feliciano Santana <fsfelicianosantana@gmail.com>
2019-07-05 21:43:09 +0000

    Update URL to deploy endpoint

--- constants.js
+++ constants.js
@@ -1,9 +1,8 @@
+export const API = {
+    URL: "http://45.79.185.51:3000"
+};
+
 export const ROLES = {
     DRIVER: "DRIVER",
     PASSENGER: "PASSENGER"
-};
-
-// TODO: Update URL
-export const API = {
-    URL: "http://192.168.0.89:3000"
 };
